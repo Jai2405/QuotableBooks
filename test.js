8a4e9aae9ad249df7efd23dc85249f8ebad58a06
Jai2405 <64653358+Jai2405@users.noreply.github.com>
2024-11-17 20:42:40 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", async (req, res) => {
+    res.send("index.html");
+    return "nope";
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
